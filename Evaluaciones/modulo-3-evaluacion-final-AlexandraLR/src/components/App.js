434c93e572c8c2adc5f8683b1b5736485d063136
Alexandra Lillo <stereodreaming@gmail.com>
2021-10-02 16:45:11 +0200

    component filter created and working

--- Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/App.js
+++ Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import getCharactersFromApi from '../services/RickAndMortyApi';
 import CharacterList from './CharacterList';
 import CharacterItem from './CharacterItem';
-// import Filters from './Filters';
+import Filters from './Filters';
 
 function App() {
   const [data, setData] = useState([]);
@@ -34,27 +34,16 @@ function App() {
       <h1 className='title--big'>Rick and Morty</h1>
       {/* IMAGEN DE RICK Y MORTY */}
       <div className='col2'>
-        {/* BUSCADOR */}
-          <input
-            className="search__character"
-            type="text"
-            name="search"
-            id="search"
-          placeholder="Encuentra a tu personaje"
-          value={searchName}
-          onChange={handleChangeSearchName}
-        />
-        {/* FILTRO */}
-        <label className='filter__species' htmlFor='species'>Busca por status</label>
-        <select className="select__filter" value={searchStatus}
-          onChange={handleChangeSearchStatus}>
-        <option value="all">Todos</option>
-        <option value="Alive">Vivo</option>
-        <option value="Dead">Muerto</option>
-        <option value="unknown">Desconocido</option>
-        </select>
+        <section>
+          <Filters
+            searchName={searchName}
+            searchStatus={searchStatus}
+            handleChangeSearchName={handleChangeSearchName}
+            handleChangeSearchStatus={handleChangeSearchStatus} />
         {/* Cards */}
         <CharacterList data={filteredData} CharacterItem={CharacterItem} />
+        
+        </section>
       </div>
     </>
   );
--- Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/Filters.js
+++ Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/Filters.js
@@ -1,2 +1,27 @@
-
+function Filters(props) {
+    return (
+        <form>
+        {/* BUSCADOR */}
+          <input
+            className="search__character"
+            type="text"
+            name="search"
+            id="search"
+          placeholder="Encuentra a tu personaje"
+          value={props.searchName}
+          onChange={props.handleChangeSearchName}
+        />
+        {/* FILTRO */}
+        <label className='filter__species' htmlFor='species'>Busca por status</label>
+        <select className="select__filter" value={props.searchStatus}
+          onChange={props.handleChangeSearchStatus}>
+        <option value="all">Todos</option>
+        <option value="Alive">Vivo</option>
+        <option value="Dead">Muerto</option>
+        <option value="unknown">Desconocido</option>
+        </select>
+        </form>
+    );
+}
+export default Filters;
   
