796c92a3180884a5d0f2abfb1835d3e60ee79130
Alexandra Lillo <stereodreaming@gmail.com>
2021-10-02 18:00:12 +0200

    add rustic route

--- Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/App.js
+++ Evaluaciones/modulo-3-evaluacion-final-AlexandraLR/src/components/App.js
@@ -1,53 +1,68 @@
-import { useState, useEffect } from 'react';
-import getCharactersFromApi from '../services/RickAndMortyApi';
-import CharacterList from './CharacterList';
-import CharacterItem from './CharacterItem';
-import Filters from './Filters';
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import getCharactersFromApi from "../services/RickAndMortyApi";
+import CharacterList from "./CharacterList";
+import CharacterItem from "./CharacterItem";
+import Filters from "./Filters";
 
 function App() {
   const [data, setData] = useState([]);
-  const [searchName, setSearchName] = useState('');
-  const [searchStatus, setSearchStatus] = useState('all');
-
+  const [searchName, setSearchName] = useState("");
+  const [searchStatus, setSearchStatus] = useState("all");
 
   useEffect(() => {
-    getCharactersFromApi().then(initialData => {
+    getCharactersFromApi().then((initialData) => {
       console.log(initialData);
       setData(initialData);
     });
-  }, []);
+  }, [searchName]);
 
   const handleChangeSearchName = (ev) => {
     setSearchName(ev.currentTarget.value);
-  }
+  };
   const handleChangeSearchStatus = (ev) => {
     setSearchStatus(ev.currentTarget.value);
-  }
-  
+  };
+
   const filteredData = data
-    .filter((data) => data.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase()))
-    .filter ((data) => searchStatus === 'all' || data.status === searchStatus
-  );
+    .filter((data) =>
+      data.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase())
+    )
+    .filter((data) => searchStatus === "all" || data.status === searchStatus);
 
   return (
     <>
-      <h1 className='title--big'>Rick and Morty</h1>
+      <h1 className="title--big">Rick and Morty</h1>
       {/* IMAGEN DE RICK Y MORTY */}
-      <div className='col2'>
-        <section>
-          <Filters
-            searchName={searchName}
-            searchStatus={searchStatus}
-            handleChangeSearchName={handleChangeSearchName}
-            handleChangeSearchStatus={handleChangeSearchStatus} />
-        {/* Cards */}
-        <CharacterList data={filteredData} CharacterItem={CharacterItem} />
-        
-        </section>
-      </div>
+
+      <Switch>
+        <Route path="/user/:id">
+          <section>Aquí va el componente card sin hacer</section>
+        </Route>
+
+        <Route exact path="/">
+          <div className="col2">
+            {/* BÚSQUEDAS */}
+            <section>
+              <Filters
+                searchName={searchName}
+                searchStatus={searchStatus}
+                handleChangeSearchName={handleChangeSearchName}
+                handleChangeSearchStatus={handleChangeSearchStatus}
+              />
+            </section>
+            {/* TARJETAS */}
+            <section>
+              <CharacterList
+                data={filteredData}
+                CharacterItem={CharacterItem}
+              />
+            </section>
+          </div>
+        </Route>
+      </Switch>
     </>
   );
 }
 
 export default App;
-
